b4d770500159815dc39f27e884edc804fbf9773d
Sergey <sergsnic@mail.ru>
2022-09-02 01:59:23 +0300

    add Seminar9Practika 3 Task

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumValues(int numbersFirst, int numbersSecond)
+{
+    if (numbersFirst == numbersSecond) return numbersSecond;
+    return numbersFirst + SumValues(numbersFirst + 1, numbersSecond);
+}
+
+Console.Write("Введите значение первого числа: ");
+int numbersFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение первого числа: ");
+int numbersSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumValues(numbersFirst, numbersSecond));
---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int AckermanFunction(int firstNumbers, int secondNumbers)
+{
+    if (firstNumbers == 0) return secondNumbers + 1;
+    if (firstNumbers > 0 & secondNumbers == 0) return AckermanFunction(firstNumbers - 1, 1);
+    return AckermanFunction(firstNumbers - 1, AckermanFunction(firstNumbers, secondNumbers - 1));
+}
+Console.Write("Введите значение первого числа: ");
+int numbersFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение первого числа: ");
+int numbersSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана({numbersFirst},{numbersSecond}) = {AckermanFunction(numbersFirst, numbersSecond)}");
